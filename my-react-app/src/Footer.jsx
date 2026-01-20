4415492eec498f9d2cca4e753992b7d7f084bead
aronlama10 <moktanaron10@gmail.com>
2026-01-20 22:36:07 +0545

    ReactJS Beginning

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Aron's first React app</p>
+        </footer>
+    );
+}
+
+export default Footer;
