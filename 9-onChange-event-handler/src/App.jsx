934aabb408ffd7cc64d59a2c64cbf457d5a32c2f
aronlama10 <moktanaron10@gmail.com>
2026-01-28 22:18:44 +0545

    onChange event handler

---
+++ 9-onChange-event-handler/src/App.jsx
@@ -0,0 +1,15 @@
+// onChange = event handler used primarily with form elements 
+//            ex. <input>, <textarea>, <select>, <radio> 
+//            Triggers a function every time the value of the input changes
+
+import MyComponent from "./MyComponent.jsx";
+
+function App() {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+}
+
+export default App
